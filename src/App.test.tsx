0b75f2628576a740c76767a2e1cda84bd712e67c
Morgan Yu <morgan.yu@createmusicgroup.com>
2021-03-11 03:02:03 -0800

    Initial commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,61 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { calculatePurchaseTotal } from "./utils";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("['grapes', 1], ['apples', 0], ['peaches', 1] ]", () => {
+    it("returns 12", () => {
+        expect(
+            calculatePurchaseTotal([
+                ["grapes", 1],
+                ["apples", 0],
+                ["peaches", 1],
+            ])
+        ).toBe(12);
+    });
+});
+
+describe("[ ['grapes', 1], ['apples', 1], ['peaches', 1] ]", () => {
+    it("returns 15", () => {
+        expect(
+            calculatePurchaseTotal([
+                ["grapes", 1],
+                ["apples", 1],
+                ["peaches", 1],
+            ])
+        ).toBe(15);
+    });
+});
+
+describe("[ ['grapes', 2], ['apples', 2], ['peaches', 1] ]", () => {
+    it("returns 16.8", () => {
+        expect(
+            calculatePurchaseTotal([
+                ["grapes", 2],
+                ["apples", 2],
+                ["peaches", 1],
+            ])
+        ).toBe(16.8);
+    });
+});
+
+describe("[ ['grapes', 3], ['apples', 5], ['peaches', 2] ]", () => {
+    it("returns 36", () => {
+        expect(
+            calculatePurchaseTotal([
+                ["grapes", 3],
+                ["apples", 5],
+                ["peaches", 2],
+            ])
+        ).toBe(36);
+    });
+});
+
+describe("[ ['peaches', 7], ['grapes', 7], ['apples', 7] ]", () => {
+    it("returns 85.8", () => {
+        expect(
+            calculatePurchaseTotal([
+                ["peaches", 7],
+                ["grapes", 7],
+                ["apples", 7],
+            ])
+        ).toBe(85.8);
+    });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,101 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useReducer } from "react";
+import { calculatePurchaseTotal } from "./utils";
+import { initialState, reducer } from "./reducer";
+
+import "./App.css";
+
+export type Item = [string, number];
+export type Items = Item[];
+
+const defaultItems: Items = [
+    ["apples", 0],
+    ["grapes", 0],
+    ["peaches", 0],
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    const [items, setItems] = useState(defaultItems);
+    const [purchaseTotal, setPurchaseTotal] = useState(0);
+
+    const resetState = () => {
+        setItems(defaultItems);
+        setPurchaseTotal(0);
+    };
+
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const updatedValues: Items = items.map((item) =>
+            item[0] === e.target.name
+                ? [item[0], parseInt(e.target.value)]
+                : item
+        );
+
+        setItems(updatedValues);
+    };
+
+    const onMakePurchase = () => {
+        dispatch({ type: "PURCHASE", items, total: purchaseTotal });
+        resetState();
+    };
+
+    return (
+        <div className="App">
+            <section>
+                <h3>Checkout Interface</h3>
+                <form
+                    onSubmit={(e) => {
+                        e.preventDefault();
+                        setPurchaseTotal(calculatePurchaseTotal(items));
+                    }}
+                >
+                    {items.map((item) => (
+                        <div key={item[0]}>
+                            <label htmlFor={`${item[0]}`}>
+                                {item[0]}
+                                <input
+                                    id={`${item[0]}`}
+                                    name={`${item[0]}`}
+                                    type="number"
+                                    min="0"
+                                    step="1"
+                                    onChange={onChange}
+                                    value={item[1]}
+                                />
+                            </label>
+                        </div>
+                    ))}
+                    <button type="submit">Calculate Total</button>
+                </form>
+                <div>Purchase Total: ${purchaseTotal}</div>
+                {purchaseTotal !== 0 && (
+                    <button type="button" onClick={onMakePurchase}>
+                        Make Purchase
+                    </button>
+                )}
+            </section>
+            <section>
+                <h3>Admin Interface</h3>
+                <div>
+                    {state.fruits.map(({ name, inventory, sold }) => (
+                        <>
+                            <h4>{name}</h4>
+                            <div>Inventory: {inventory}</div>
+                            <div>Sold: {sold}</div>
+                        </>
+                    ))}
+                </div>
+                <h4>Purchase History</h4>
+                {state.purchaseRecord?.map(({ date, purchaseTotal }) => (
+                    <p>
+                        <span>Purchase Time: {date}</span> |
+                        <span>Purchase: Total: {purchaseTotal}</span>
+                    </p>
+                ))}
+                <h3>Net Profit: {state.netProfit}</h3>
+            </section>
+        </div>
+    );
 }
 
 export default App;
